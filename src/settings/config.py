48c9cbf9c249bc9b614365f1b451e88699d93e0a
Alexey <porsh-5@yandex.ru>
2024-04-01 23:20:41 +0300

    Add exp config

--- src/settings/config.py
+++ src/settings/config.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from src.settings.base import ExtraFieldsNotAllowedBaseModel
 from src.settings.classifier import ClassifierSettings
 from src.settings.preprocessor import PreprocessorSettings
@@ -8,3 +6,9 @@ from src.settings.preprocessor import PreprocessorSettings
 class Config(ExtraFieldsNotAllowedBaseModel):
     preprocessing_config: PreprocessorSettings
     classifier_config: ClassifierSettings
+
+class ExperimentConfig(Config):
+    exp_name: str
+    log_path: str
+    model_save_path: str
+    vectorizer_save_path: str
